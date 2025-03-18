8ab039c0198e105c42d6386ae247ce4c7efb39b2
ziheng.zhang <ziheng.zhang@estudiantat.upc.edu>
2025-03-18 11:56:07 +0100

    Initial commit

---
+++ Player.java
@@ -0,0 +1,31 @@
+public class Player
+{
+    protected int id;
+    private String password;
+
+    public Player(int id,String password)
+    {
+        //CheckID(id);
+        setID(id);
+        setPassword(password);
+    }
+
+    public void setID(int id)
+    {
+        this.id = id;
+    }
+    private void setPassword(int password)
+    {
+        this.password = password;
+    }
+
+    public void getID()
+    {
+        return id;
+    }
+
+    public void changePassword(String new_pw)
+    {
+        if(password.equals(new_pw))
+    }
+}
