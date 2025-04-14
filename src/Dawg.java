b1d13cac098b65a2beaa3b448343f7bb94c20a8a
Ivan.Alcubierre <ivan.alcubierre@estudiantat.upc.edu>
2025-04-14 13:54:38 +0200

    dawg commit 1 and dicctionary commit 1

--- src/Dawg.java
+++ src/Dawg.java
@@ -5,28 +5,25 @@ public class Dawg
     //Atributos del DAWG
     public class Node
     {
-        private String id; // Identificador del nodo
-        private Set<Node> nxtNodes ; // Mapa identificador del nodo actual y puntero que apunta a los siguientes nodos
+        private Map<String, Node> children ; // Mapa identificador del nodo actual y puntero que apunta a los siguientes nodos
         private boolean isFinalNode; // Indica si el nodo es terminal
 
-        public Node(String id, boolean isFinalNode)
+        public Node(boolean isFinalNode)
         {
             // Esta constructora necesita como parámetros el id del nodo y un booleano, no sé si son necesarios los dos parámetros.
-            this.id = id;
-            this.nxtNodes = new HashSet<>();
+            this.children = new HashMap<String, Node>();
             this.isFinalNode = isFinalNode;
-            
         }
 
-        public String getId()
+        public boolean isFinal()
         {
-            return id;
+            return isFinalNode;
         }
 
-        public void addNextNode(Node node)
+        public void addNextNode(String nextLetter, Node node)
         {
             // Añadimos un nodo al conjunto de nodos siguientes
-            nxtNodes.add(node);
+            children.put(nextLetter, node);
         } //añdairé remove solo si es necesario
 
         public void changeState()
@@ -36,11 +33,109 @@ public class Dawg
 
     }
 
+    //Atributos del DAWG
+    private String language; // Idioma del DAWG
     private Node root; // Nodo raíz del DAWG
     //... faltan funciones y atributos 
 
     public Dawg()
     {
-        // Create a new dictionary
+        root = new Node(false); // Inicializamos el nodo raíz
+    }
+
+    public void addWord(String word)
+    {
+        // Añadimos una palabra al DAWG recursivamente
+        addWordRec(root, word, 0);
+    }
+
+    // pre: word es la palabra a introducir al DAWG
+    // post: devuelve true si la siguiente letra puede ser un caracter especial, false en caso contrario
+    private boolean specialCharacter(String word, int index) 
+    {
+        if(index == word.length() - 1) return false;
+
+        if(language == "es") {
+
+            if(word.charAt(index) == 'c' && word.charAt(index + 1) == 'h') return true; // Comprobamos si el caracter es una 'ch'
+            if(word.charAt(index) == 'l' && word.charAt(index + 1) == 'l') return true; // Comprobamos si el caracter es una 'll'
+            if(word.charAt(index) == 'r' && word.charAt(index + 1) == 'r') return true; // Comprobamos si el caracter es una 'rr'
+        }
+
+        if(language == "cat") {
+
+            if(word.charAt(index) == 'l' && word.charAt(index + 1) == '·') return true; // Comprobamos si el caracter es una 'l·l'
+            if(word.charAt(index) == 'n' && word.charAt(index + 1) == 'y') return true; // Comprobamos si el caracter es una 'ny'
+        }
+
+        return false;
+    }
+
+    private void addWordRec(Node node, String word, int index)
+    {
+        // Caso Base
+        if (index == word.length())
+        {
+            node.changeState(); // Cambiamos el estado del nodo a final
+            return;
+        }
+
+        // Caso Recursivo
+        boolean isSpecial = specialCharacter(word, index);
+        if(isSpecial && language == "es") {
+
+            String c1 = String.valueOf(word.charAt(index));
+            String c2 = String.valueOf(word.charAt(index + 1));
+
+            Node nextNode1 = node.children.get(c1);
+            Node nextNode2 = node.children.get(c1 + c2);
+            if(nextNode1 == null) {
+                nextNode1 = new Node(false);
+                node.addNextNode(c1, nextNode1);
+            }
+            if(nextNode2 == null) {
+                nextNode2 = new Node(false);
+                node.addNextNode(c1 + c2, nextNode2);
+            }
+            addWordRec(nextNode1, word, index + 1);
+            addWordRec(nextNode2, word, index + 2);
+        }
+        else if(isSpecial && language == "cat") {
+
+            if(word.charAt(index) == 'l') {
+                Node nextNode = node.children.get("l·l");
+                if(nextNode == null) {
+                    nextNode = new Node(false);
+                    node.addNextNode("l·l", nextNode);
+                }
+                addWordRec(nextNode, word, index + 3);
+            }
+            else {
+                Node nextNode1 = node.children.get("n");
+                Node nextNode2 = node.children.get("ny");
+                if(nextNode1 == null) {
+                    nextNode1 = new Node(false);
+                    node.addNextNode("n", nextNode1);
+                }
+                if(nextNode2 == null) {
+                    nextNode2 = new Node(false);
+                    node.addNextNode("ny", nextNode2);
+                }
+                addWordRec(nextNode1, word, index + 1);
+                addWordRec(nextNode2, word, index + 2);
+            }
+        }
+        else {
+            String letter = String.valueOf(word.charAt(index));
+            Node nextNode = node.children.get(letter);
+
+            if (nextNode == null)
+            {
+                nextNode = new Node(false);
+                node.addNextNode(letter, nextNode);
+            }
+
+            addWordRec(nextNode, word, index + 1);
+        }
     }
 }
--- src/Dictionary.java
+++ src/Dictionary.java
@@ -2,8 +2,50 @@ import java.util.*;
 
 public class Dictionary
 {
+
+    //Atributos del diccionario
+    String name; // Nombre del diccionario
+    String language; // Idioma del diccionario
+    Dawg dawg; // Estructura de datos que contiene el DAWG
+
     public Dictionary()
     {
         // Create a new dictionary
+    }
+
+    public Dictionary(String language)
+    {
+        // Create a new dictionary with the specified language
+        this.language = language;
+        this.dawg = new Dawg();
+    }
+
+    public createDictionary(String language)
+    {
+        // Create a new dictionary with the specified language
+        new Dictionary(language);
+        ///////// leer el archivo de texto y añadir las palabras al DAWG
+    }
+
+    public void addWord(String word)
+    {
+        // Add a word to the dictionary
+        dawg.addWord(word);
+    }
+
+    public void removeWord(String word)
+    {
+        // Remove a word from the dictionary
+        dawg.removeWord(word);
+    }
+
+    //pre: rack es una cadena de caracteres que representa las letras disponibles
+    //post: devuelve una cadena de caracteres que representa la palabra encontrada en el diccionario
+    //       o null si no se encuentra ninguna palabra
+    //       (en caso de que haya más de una palabra, devuelve la primera que encuentra)
+    public String searchWord(String rack)
+    {
+        // Search for a word in the dictionary
+        return dawg.findWord(rack);
     }
 }
