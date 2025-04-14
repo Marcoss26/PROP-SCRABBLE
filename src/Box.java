c7dfc46d3d2506ee6f4d76d7fc6c5eb2bd3678d6
alvaro.perez.criado <alvaro.perez.criado@estudiantat.upc.edu>
2025-04-14 17:53:32 +0200

    modificado clase casilla

--- src/Box.java
+++ src/Box.java
@@ -2,8 +2,52 @@ import java.util.*;
 
 public class Box
 {
-    public Box()
+    private int x;
+    private int y;
+    private boolean isOccupied; //pendiente de borrar, con idFicha ya
+                                //sabemos si está ocupada o no
+    private String idFicha;
+
+    public void applyMultiplicator();
+
+    public class DoubleLetter extends Box
     {
-        // Create a new board
+        private int multiplicator = 2;
+
+        @Override
+        public void applyMultiplicator()
+        {
+            // Implementación del multiplicador de letra doble
+        }
+    }
+    public class TripleLetter extends Box
+    {
+        private int multiplicator = 3;
+
+        @Override
+        public void applyMultiplicator()
+        {
+            // Implementación del multiplicador de letra triple
+        }
+    }
+    public class DoubleWord extends Box
+    {
+        private int multiplicator = 2;
+
+        @Override
+        public void applyMultiplicator()
+        {
+            // Implementación del multiplicador de palabra doble
+        }
+    }
+    public class TripleWord extends Box
+    {
+        private int multiplicator = 3;
+
+        @Override
+        public void applyMultiplicator()
+        {
+            // Implementación del multiplicador de palabra triple
+        }
     }
 }
