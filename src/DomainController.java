520f3bbef58caca7d2ce8163f3855e812db55596
Kai K <kai@kaiknox.com>
2025-04-16 03:23:19 +0200

    Add methods to return sub-controllers

--- src/DomainController.java
+++ src/DomainController.java
@@ -2,22 +2,36 @@ import java.util.*;
 
 public class DomainController {
     private ProfileController profileController;
+    private MatchController MP_Controller;
 
     public DomainController() {
+        this.profileController = new ProfileController();
+        this.MP_Controller = new MatchController();
     }
 
+    /* ---------------------------------------------------------------------
+                            PROFILE FUNCTIONALITY
+    ------------------------------------------------------------------------*/
+    public void manageProfiles() {
+        return this.profileController;
+    }
     public void addProfile(String username, String password) {
-        profileController.addProfile(username, password);
+        this.profileController.addProfile(username, password);
     }
     public void removeProfile(String username) {
-        profileController.removeProfile(username);
+        this.profileController.removeProfile(username);
     }
     public Profile getProfile(String username) {
-        return profileController.getProfile(username);
+        return this.profileController.getProfile(username);
     }
 
-    public void newGame() {
-        MP_Controller.createMatch("matchID", 2, 2);
+    /* ---------------------------------------------------------------------
+                            MATCH FUNCTIONALITY
+    ------------------------------------------------------------------------*/
+    public void manageMatches() {
+        return this.MP_Controller;
     }
-    
+    public void newMatch(matchID, int humanCount, int aiCount) {
+        this.MP_Controller.createMatch(matchID, humanCount, aiCount);
+    }
 }
