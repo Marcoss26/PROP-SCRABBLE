95eeb5941bc22015e7dcfc92fa5a84373906d935
alvaro.perez.criado <alvaro.perez.criado@estudiantat.upc.edu>
2025-04-22 20:18:39 +0200

    retoque en Board

--- src/DomainClasses/Board.java
+++ src/DomainClasses/Board.java
@@ -236,13 +236,22 @@ public class Board
         }
     }
 
-    /**
-     * Retorna las casillas ancla del tablero
-     * Pre: ya existe un tablero
-     * @return El conjunto de casillas ancla
-     */
 
-    public Set<Box> getAnchorSquares() {
+     public Set<Box> getLeftAnchorSquares() {
+        Set<Box> anchorSquares = new HashSet<>();
+    
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (hasLetter(i, j)) {
+                    addAnchorSquareIfValid(anchorSquares, i, j - 1); // Izquierda
+                }
+            }
+        }
+    
+        return anchorSquares;
+    }
+
+    public Set<Box> getToptAnchorSquares() {
         Set<Box> anchorSquares = new HashSet<>();
     
         for (int i = 0; i < size; i++) {
@@ -250,9 +259,6 @@ public class Board
                 if (hasLetter(i, j)) {
                     // Revisar las casillas adyacentes
                     addAnchorSquareIfValid(anchorSquares, i - 1, j); // Arriba
-                    addAnchorSquareIfValid(anchorSquares, i + 1, j); // Abajo
-                    addAnchorSquareIfValid(anchorSquares, i, j - 1); // Izquierda
-                    addAnchorSquareIfValid(anchorSquares, i, j + 1); // Derecha
                 }
             }
         }
@@ -268,7 +274,7 @@ public class Board
      * @param y
      */
 
-    private void addAnchorSquareIfValid(Set<Box> anchorSquares, int x, int y) { //me guastaria cambiar el nombre
+    private void addAnchorSquareIfValid(Set<Box> anchorSquares, int x, int y) {
         if (x >= 0 && x < size && y >= 0 && y < size && !hasLetter(x, y)) {
             anchorSquares.add(board[x][y]);
         }
@@ -282,7 +288,36 @@ public class Board
      * @return La letra de la casilla correspondiente a los valores de entrada
      */
 
-    public char getLetter(int x, int y) { //deberia ser un string en vez de un char
-        return board[x][y].getSymbol().charAt(0);
+    public String getLetter(int x, int y) {
+        return board[x][y].getSymbol();
+    }
+
+    public Box getLeftNeighbor(Box anchor) {
+        if (anchor.getY() > 0) {
+            return board[anchor.getX()][anchor.getY() - 1];
+        }
+        return null;
+    }
+
+    public Box getRightNeighbor(Box anchor) {
+        if (anchor == null) return null;
+        if (anchor.getY() < size - 1) {
+            return board[anchor.getX()][anchor.getY() + 1];
+        }
+        return null;
+    }
+
+    public Box getTopNeighbor(Box anchor) {
+        if (anchor.getX() > 0) {
+            return board[anchor.getX() - 1][anchor.getY()];
+        }
+        return null;
+    }
+
+    public Box getDownNeighbor(Box anchor) {
+        if (anchor.getX() < size - 1) {
+            return board[anchor.getX() + 1][anchor.getY()];
+        }
+        return null;
     }
 }
