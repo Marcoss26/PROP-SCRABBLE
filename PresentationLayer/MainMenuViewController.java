03d0a2f27f99275eddfa080a623815c243f11c21
marcos.arroyo.saavedra <marcos.arroyo.saavedra@estudiantat.upc.edu>
2025-05-03 15:06:42 +0200

    Starting Presentation Layer

---
+++ PresentationLayer/MainMenuViewController.java
@@ -0,0 +1,10 @@
+package PresentationLayer;
+import javafx.fxml.FXML;
+
+public class MainMenuViewController {
+
+    @FXML
+    private void handleButtonClick() {
+        System.out.println("¡Botón pulsado!");
+    }
+}
