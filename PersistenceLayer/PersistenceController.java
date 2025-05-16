0309e8e3f464e491442e98f6426163244e23279c
Kai K <kai@kaiknox.com>
2025-05-17 01:35:22 +0200

    Add more methods for persistence layer

--- PersistenceLayer/PersistenceController.java
+++ PersistenceLayer/PersistenceController.java
@@ -7,10 +7,10 @@ import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.util.*;
+import java.util.Base64;
 
 
 class JsonUtils {
-
     // Save a JSONArray to a file
     public static void saveJsonToFile(String filePath, JSONArray jsonArray) {
         try (FileWriter file = new FileWriter(filePath)) {
@@ -34,6 +34,7 @@ class JsonUtils {
         return new JSONArray(); // Return an empty array if the file doesn't exist or an error occurs
     }
 }
+
 
 
 public class PersistenceController {
@@ -102,5 +103,52 @@ public class PersistenceController {
 
 
 
-    
+
+
+    /*
+     * ---------------------------------------------------------------------
+                            MATCH FUNCTIONALITY
+     ------------------------------------------------------------------------
+     */
+    public void saveMatchProfiles(Map<String, Profile> profiles) {
+        JSONArray profilesArray = new JSONArray();
+
+        for (Map.Entry<String, Profile> entry : profiles.entrySet()) {
+            JSONObject profileObject = new JSONObject();
+            profileObject.put("username", entry.getKey());
+            profileObject.put("password", entry.getValue().getPassword());
+            profileObject.put("gamesPlayed", entry.getValue().getGamesPlayed());
+            profileObject.put("wins", entry.getValue().getWins());
+            profileObject.put("score", entry.getValue().getScore());
+            profilesArray.add(profileObject);
+        }
+
+        JsonUtils.saveJsonToFile(DATA_PATH + "/profiles.json", profilesArray);
+    }
+
+    public Map<String, Profile> loadMatches() {
+        Map<String, Profile> profiles = new HashMap<>();
+        JSONArray profilesArray = JsonUtils.loadJsonFromFile(DATA_PATH + "/profiles.json");
+
+        for (Object obj : profilesArray) {
+            JSONObject profileObject = (JSONObject) obj;
+            String username = (String) profileObject.get("username");
+            String password = (String) profileObject.get("password");
+            long gamesPlayed = (long) profileObject.get("gamesPlayed");
+            long wins = (long) profileObject.get("wins");
+            long score = (long) profileObject.get("score");
+
+            Profile profile = new Profile(username, password);
+            profile.setGamesPlayed((int) gamesPlayed);
+            profile.setWins((int) wins);
+            profile.addScore((int) score);
+
+            profiles.put(username, profile);
+        }
+
+        return profiles;
+    }
+
+
+
 }
