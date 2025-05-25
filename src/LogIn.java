0428f59286098b4a597b8db283bc5f7f65995ba8
ziheng.zhang <ziheng.zhang@estudiantat.upc.edu>
2025-05-25 16:47:51 +0200

    src folder elimination

--- src/LogIn.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class LogIn {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton SIGNUPButton;
-    private JButton LOGINButton;
-    private JButton CANCELButton;
-}
